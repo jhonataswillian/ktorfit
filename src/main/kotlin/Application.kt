872796cc00497e3b3948a944be7f51b46a88925b
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-02-14 16:21:34 -0300

    feat: implement exercise routes and wiring

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,7 +4,9 @@ import com.example.plugins.configureDatabases
 import com.example.plugins.configureMonitoring
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
+import com.example.repositories.ExposedExerciseRepository
 import com.example.repositories.ExposedWorkoutRepository
+import com.example.routes.exerciseRoutes
 import com.example.routes.workoutRoutes
 import io.ktor.server.application.*
 import io.ktor.server.routing.routing
@@ -19,8 +21,11 @@ fun Application.module() {
     configureMonitoring()
     configureRouting()
 
-    val repository = ExposedWorkoutRepository()
+    val workoutRepository = ExposedWorkoutRepository()
+    val exerciseRepository = ExposedExerciseRepository()
+
     routing {
-        workoutRoutes(repository)
+        workoutRoutes(workoutRepository)
+        exerciseRoutes(exerciseRepository)
     }
 }
---
+++ src/main/kotlin/routes/ExerciseRoutes.kt
@@ -0,0 +1,57 @@
+package com.example.routes
+
+import com.example.models.Exercise
+import com.example.repositories.ExerciseRepository
+import io.ktor.http.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun Route.exerciseRoutes(repository: ExerciseRepository) {
+
+    route("/exercises") {
+
+        // Criar Exercício
+        post {
+            try {
+                val exercise = call.receive<Exercise>()
+                val newExercise = repository.addExercise(exercise)
+                call.respond(HttpStatusCode.Created, newExercise)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest,
+                    "Error creating exercise: ${e.localizedMessage} ")
+            }
+        }
+
+        // Deletar Exercício
+        delete("/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid ID")
+                return@delete
+            }
+
+            val deleted = repository.deleteExercise(id)
+            if (deleted) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Exercise Not Found")
+            }
+        }
+    }
+
+    route("/workouts/{id}/exercises") {
+        get {
+            val workoutId = call.parameters["id"]?.toIntOrNull()
+            if (workoutId == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid Workout ID")
+                return@get
+            }
+
+            val exercises = repository.exercisesByWorkout(workoutId)
+            call.respond(HttpStatusCode.OK, exercises)
+        }
+    }
+
+}
